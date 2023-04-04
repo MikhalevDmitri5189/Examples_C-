6f2adb2af3761c309648dc52a605b2fdd75403d3
Dmitrii Mikhalev <mixalev89@mail.ru>
2023-04-04 16:27:57 +0800

    Get Random

--- Example03_Dif/Program.cs
+++ Example03_Dif/Program.cs
@@ -2,6 +2,13 @@
 int numberB = 5;
 Console.WriteLine(numberA / numberB);
  */
-double numberA = 12;
+/* double numberA = 12;
 double numberB = 5;
-Console.WriteLine(numberA / numberB);
+Console.WriteLine(numberA / numberB); */
+
+int numberA = new Random().Next(1, 10);// В переменную numderA с генерируется случайное число от 1 до 10.
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
