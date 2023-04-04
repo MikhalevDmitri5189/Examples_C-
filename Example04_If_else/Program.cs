e771987d44f2837f2fa7f11d0955ed38ac32f667
Dmitrii Mikhalev <mixalev89@mail.ru>
2023-04-04 17:00:04 +0800

    If_Else added

---
+++ Example04_If_else/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша") // ToLower переводит все буквы в нижний регистр.
+                                  // Можно вводить имя Маша с большой или мальнькой буквы.
+{
+    Console.WriteLine("Ура это же Маша");
+}
+
+else
+{
+    Console.WriteLine("Привет,");
+    Console.WriteLine(username);
+}
+
+
+
